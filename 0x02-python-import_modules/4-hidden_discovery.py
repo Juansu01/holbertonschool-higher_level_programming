e422a246688595bd95a53a2151e6373d90024283
Juansu01 <thejuank0@hotmail.com>
2021-04-28 21:56:06 +0000

    Added script that prints names

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import hidden_4
+
+    directories = dir(hidden_4)
+    
+    for i in range(0, len(directories)):
+        if directories[i][0:1] != "_":
+            print(directories[i])

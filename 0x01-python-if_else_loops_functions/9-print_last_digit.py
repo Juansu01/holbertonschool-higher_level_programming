1132211fc3b5045ae1871406311bc798bd8768c4
Juansu01 <thejuank0@hotmail.com>
2021-05-04 06:18:18 -0500

    Added function that prints last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+
+    if number > 0:
+        last = number % 10
+
+    elif number == 0:
+        last = 0
+
+    else:
+        last = (number * -1) % 10
+
+    print("{}".format(last), end='')
+    return last

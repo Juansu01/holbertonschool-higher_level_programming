0d5d3e245a62942e8480f9064603bd1285f41e92
Juansu01 <thejuank0@hotmail.com>
2021-05-05 13:31:14 -0500

    Fixed spaces  asa
    as

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-
 def no_c(my_string):
 
     nocstring = ""
@@ -9,5 +7,4 @@ def no_c(my_string):
         if i == "c" or i == "C":
             i = ""
         nocstring += i
-
     return nocstring

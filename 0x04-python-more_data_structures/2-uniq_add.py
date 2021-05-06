940e79afba42d1baf817d80ab147420e204159d9
Juansu01 <thejuank0@hotmail.com>
2021-05-06 16:06:00 -0500

    Testing changes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,13 +3,10 @@
 
 def uniq_add(my_list=[]):
 
-    if not my_list:
-        return []
-    else:
-        list_unique_numbers = []
-        unique_numbers = set(my_list)
+    list_unique_numbers = []
+    unique_numbers = set(my_list)
 
-        for number in unique_numbers:
-            list_unique_numbers.append(number)
+    for number in unique_numbers:
+        list_unique_numbers.append(number)
 
-        return sum(list_unique_numbers)
+    return sum(list_unique_numbers)

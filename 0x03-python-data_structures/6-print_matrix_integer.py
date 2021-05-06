81ca910df61934133193d9b6626883a877f085fd
Juansu01 <thejuank0@hotmail.com>
2021-05-06 13:54:47 -0500

    Added space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,5 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    print('\n'.join([''.join(['{:3}'.format(item) for item in row]) 
-      for row in matrix]))
+    print('\n'.join([' '.join(['{}'.format(item) for item in row])for row in matrix]))
         

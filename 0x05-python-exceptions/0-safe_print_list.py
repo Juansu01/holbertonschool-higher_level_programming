0a0e73648f824f60c3315f7ce8af2bc508d8766b
Juansu01 <thejuank0@hotmail.com>
2021-05-17 12:14:43 -0500

    Changed code style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,10 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(0, x):
-            print(my_list[i], end = "")
+            print(my_list[i], end="")
             count += 1
         print("")
-    except:
+    except IndexError:
         print("")
     finally:
         return count

7ff6e480512503cb608f4dd3c6cac9497133b6bc
Juansu01 <thejuank0@hotmail.com>
2021-05-18 16:25:15 -0500

    Trying changes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,6 @@ Class defining a square
 
 
 class Square():
-    __size = None
     """ This class defines a private size for a Square """
-
     def __init__(self, __size=None):
-        if __size is not None:
-            self.__size = __size
+        self.__size = __size

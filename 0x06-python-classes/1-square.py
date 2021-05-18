3d681c97d7ebf32a34021c7be773ea73e4590b7c
Juansu01 <thejuank0@hotmail.com>
2021-05-18 16:24:04 -0500

    Removed comment

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,6 +9,5 @@ class Square():
     """ This class defines a private size for a Square """
 
     def __init__(self, __size=None):
-        """ This function assigns the size the the object """
         if __size is not None:
             self.__size = __size

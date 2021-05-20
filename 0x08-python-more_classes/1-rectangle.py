7144ce484e5bc4655d4cd8e0f5c14bb08904308f
Juansu01 <thejuank0@hotmail.com>
2021-05-20 16:52:23 -0500

    Added two new tasks

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This class defines a rectangle with a width and height
+"""
+
+
+class Rectangle():
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self, value):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+    @property
+    def width(self, value):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This class defines a rectangle with a width and height
+"""
+
+
+class Rectangle():
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self, value):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+    @property
+    def width(self, value):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width + self.__height + self.__width + self.__height

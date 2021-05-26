faafa87839baca90b122684e521a71694bb7cd30
Juansu01 <thejuank0@hotmail.com>
2021-05-26 09:27:59 -0500

    Added flag to ignore spaces after new lines

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,14 +7,21 @@ these characters: ., ? and :.
 
 def text_indentation(text):
     '''This function prints two new lines after every: ., ? and :. '''
+    flag = 0
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in text:
         if i == ".":
+            flag = 1
             print("{}\n".format(i))
         elif i == ":":
+            flag = 1
             print("{}\n".format(i))
         elif i == "?":
+            flag = 1
             print("{}\n".format(i))
         else:
+            if flag == 1:
+                flag = 0
+                continue
             print(i, end="")

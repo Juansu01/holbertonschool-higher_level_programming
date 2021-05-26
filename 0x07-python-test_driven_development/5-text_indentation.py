f90108111bd2a8e740bffcc412033f5a4c383595
Juansu01 <thejuank0@hotmail.com>
2021-05-26 07:51:43 -0500

    Fixed spaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,13 +10,13 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in text:
-        print("{}".format(i), end="")
         if i == ".":
-            print("")
-            print("")
-        if i == ":":
-            print("")
-            print("")
-        if i == "?":
-            print("")
-            print("")
+            print("{}\n".format(i))
+        elif i == ":":
+            print("{}\n".format(i))
+        elif i == "?":
+            print("{}\n".format(i))
+        else:
+            print(i, end="")
+
+

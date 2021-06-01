2b7609bf6ee5dab5e7635bf350ce6ad322c41594
Juansu01 <thejuank0@hotmail.com>
2021-06-01 18:53:39 -0500

    Forgot to initialize using the init method and super

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 class MyList(list):
     """This class defines a method that sorts a list."""
 
+    def __init__(self):
+        """ Initializing from inherited class"""
+        super().__init__()
+
     def print_sorted(self):
         """ Don't mind me, just sorting a list and printing it."""
         print(sorted(self))

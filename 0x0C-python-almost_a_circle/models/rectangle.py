1b6b0d70c029ad945c25557083f8d0e3a7e5fe0c
Juansu01 <thejuank0@hotmail.com>
2021-06-08 10:18:17 -0500

    Added getters and setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,32 +16,60 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ This is the width getter."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.__width = value
+        """ This is the width setter."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
+        """ This is the height getter."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.__height = value
+        """ This is the height setter."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
+        """ This is the x getter."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.__x = value
+        """ This is the x setter."""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
+        """ This is the x getter."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.__y = value
+        """ This is the y setter."""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value

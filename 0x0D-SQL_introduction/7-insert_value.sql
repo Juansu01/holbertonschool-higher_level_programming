ed5b2808dcb64f62216f4d9a7a54750831c31473
Juansu01 <thejuank0@hotmail.com>
2021-07-12 15:56:16 -0500

    Simplified command

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
 -- This script inserts an id and name to the table.
-INSERT INTO first_table (id)
-VALUES (89);
-INSERT INTO first_table (name)
-VALUES ('Holberton School');
+INSERT INTO first_table (id, name)
+VALUES (89, 'Holberton School');

592e9bb725cfc688cae832ea9285c8fe8f6011d5
Juansu01 <thejuank0@hotmail.com>
2021-07-12 16:28:57 -0500

    Added order by score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- This script displays integrants who have a score greater than 10.
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

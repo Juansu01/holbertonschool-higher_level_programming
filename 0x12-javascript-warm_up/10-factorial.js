9797ffde5438c9362a4f844a72d5e9587951f2e1
Juansu01 <thejuank0@hotmail.com>
2021-08-02 14:50:10 -0500

    Added a script that calculates the factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2]);
+
+function factorialize (num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return (num * factorialize(num - 1));
+  }
+}
+
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('NaN');
+} else {
+  console.log(factorialize(a));
+}

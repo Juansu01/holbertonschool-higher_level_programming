2058e70cad86ef72ad16d24ab5ceff3ed9b42079
Juansu01 <thejuank0@hotmail.com>
2021-08-02 15:57:15 -0500

    Fixed index and style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,19 +4,17 @@ let biggest = 0;
 let secondBiggest = 0;
 
 if (process.argv.length === 2) {
-    console.log('0');
-    return;
-  } else if (process.argv.length === 3) {
-    console.log('0');
-    return;
-  } else {
-    for (let i = 3; i < process.argv.length; i++) {
-        if (process.argv[i] > biggest) {
-            secondBiggest = biggest;
-            biggest = process.argv[i];
-        } else {
-            continue;
-        }
+  console.log('0');
+} else if (process.argv.length === 3) {
+  console.log('0');
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] > biggest) {
+      secondBiggest = biggest;
+      biggest = process.argv[i];
+    } else {
+      continue;
     }
   }
-console.log(secondBiggest);
+  console.log(secondBiggest);
+}

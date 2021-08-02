27fff24cc2aee3d37099567d72234998ff29e891
Juansu01 <thejuank0@hotmail.com>
2021-08-02 15:49:26 -0500

    Added task #11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+let biggest = 0;
+let secondBiggest = 0;
+
+if (process.argv.length === 2) {
+    console.log('0');
+    return;
+  } else if (process.argv.length === 3) {
+    console.log('0');
+    return;
+  } else {
+    for (let i = 3; i < process.argv.length; i++) {
+        if (process.argv[i] > biggest) {
+            secondBiggest = biggest;
+            biggest = process.argv[i];
+        } else {
+            continue;
+        }
+    }
+  }
+console.log(secondBiggest);

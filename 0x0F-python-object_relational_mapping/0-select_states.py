12def05ba740c444cc5dd47609c033506e507339
Juansu01 <thejuank0@hotmail.com>
2021-08-06 11:21:00 -0500

    script that lists states in a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=u, passwd=p, db=d, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC ")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

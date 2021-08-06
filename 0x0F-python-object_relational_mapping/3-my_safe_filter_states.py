36f418de1fb3b3492b78b6944313e488b3e050f3
Juansu01 <thejuank0@hotmail.com>
2021-08-06 13:04:37 -0500

    Added script that displays states safely

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module lists all states starting with N
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    uinput_state = sys.argv[4]
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=u, passwd=p, db=d, charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states \
+        WHERE name like %(state)s \
+        ORDER BY id ASC", {'state': uinput_state})
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

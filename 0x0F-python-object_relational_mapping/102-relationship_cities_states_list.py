497e57878a47e87045d64ff5fd2b46ec67dcbd6f
Juansu01 <thejuank0@hotmail.com>
2021-08-08 11:59:21 -0500

    Trying advanced task

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    cities_and_states = session.query(City, State).filter(
+        State.id == City.state_id).all()
+    for city, state in cities_and_states:
+        print("{}: {} -> {}".format(city.id, city.name, state.name))

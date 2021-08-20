3c575f5668b254efedbb4622c323689213017346
juansu01 <thejuank0@hotmail.com>
2021-08-20 14:00:14 -0500

    Fixed task, using different module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """This module shows a status """
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        resp = response.read()
-        decoded = resp.decode('UTF-8')
-        print('Body response:')
-        print('\t- type: {}'.format(type(decoded)))
-        print('\t- content: {}'.format(decoded))
+    import requests
+    resp = requests.get('https://intranet.hbtn.io/status')
+    content = resp.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))

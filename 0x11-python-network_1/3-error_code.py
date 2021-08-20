319e24ea8ae39a7e031685140fda47cb9f1a35ce
juansu01 <thejuank0@hotmail.com>
2021-08-20 13:30:51 -0500

    Added script that handles erros

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module handles errors """
+
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            resp = response.read()
+            print(resp.decode('utf-8'))
+    except urllib.error.HTTPError as my_err:
+        print('Error code: {}'.format(my_err))

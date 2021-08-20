d7bd784ed86cc106cf20e81d22a3bd462ef92700
juansu01 <thejuank0@hotmail.com>
2021-08-20 14:31:36 -0500

    Adding permissions

--- 0x11-python-network_1/6-post_email.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module shows a status """
-if __name__ == "__main__":
-    from sys import argv
-    import requests
-    ema = {'email': argv[2]}
-    r = requests.post(argv[1], data=ema)
-    print(r.text)

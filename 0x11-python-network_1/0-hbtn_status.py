9735aab3244e4dc2f088e172d462ce3daac209d5
juansu01 <thejuank0@hotmail.com>
2021-08-20 12:20:27 -0500

    Adding first task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        resp = response.read()
+        decoded = resp.decode('UTF-8')
+        print('Body response:')
+        print('\t- type: {}'.format(type(resp)))
+        print('\t- content: {}'.format(resp))
+        print('\t- utf8 content: {}'.format(decoded))

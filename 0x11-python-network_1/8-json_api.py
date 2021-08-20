9ab015d5ec26cbff9378266e8472778653ba11ab
juansu01 <thejuank0@hotmail.com>
2021-08-20 15:05:02 -0500

    Added a script that posts to an api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module sends a post request and displays results if found """
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if argv[1]:
+        da = {'q': argv[1]}
+    else:
+        da = {'q': ''}
+
+    try:
+        req = requests.post('http://0.0.0.0:5000/search_user', data=da)
+        my_json = req.json()
+        if 'id' in my_json and 'name' in my_json:
+            print("[{}] {}".format(my_json.get('id'), my_json.get('name')))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")

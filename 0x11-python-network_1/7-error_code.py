c303c74d8f0bf6a6ade73b77351876fca98bab91
juansu01 <thejuank0@hotmail.com>
2021-08-20 14:37:31 -0500

    Adding error handling using requests module

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import request
+    from sys import argv
+    from requests.exceptions import HTTPError
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code > 400:
+        print('Error code: {}'.format(req.status_code))
+    else:
+        print(r.text)

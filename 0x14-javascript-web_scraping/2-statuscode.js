db4d8cb0c6b6bfdcd2c22d6d2919f56eda8d2f06
juansu01 <thejuank0@hotmail.com>
2021-09-14 13:03:02 -0500

    Using request this time

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
-const https = require('https');
+const https = require('request');
 const webPage = process.argv;
 
-https.get(webPage[2], function (res) {
+https.get(webPage[2], function (err, res) {
+  if (err) {
+    console.error(err);
+  }
   console.log('code: ' + res.statusCode);
 });

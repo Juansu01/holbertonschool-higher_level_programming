37aa37035f28a5429cde614c3da85238ee2e19c0
juansu01 <thejuank0@hotmail.com>
2021-09-14 12:52:52 -0500

    Adding third task, get status code of a get resquest

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const https = require('https');
+const webPage = process.argv;
+
+https.get(webPage[2], function (res) {
+  console.log('code:', res.statusCode); // <======= Here's the status code
+});
